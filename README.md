f6704d7b5bc47cc3efa03b86c9730f6cd25c8442
Logan-bobo <logan.bobofed@gmail.com>
2022-01-04 20:44:40 +0000

    correcting creation of snapshots and starting KMS key management

--- README.md
+++ README.md
@@ -1,56 +1,56 @@
 # RDS Encrypt
-A tool used to encrypt RDS instances that were previously created in an unencrypted state 
+
+A tool used to encrypt RDS instances that were previously created in an unencrypted state
 
 Currently it is a strenuous process to encrypt an unencrypted RDS instance. This tool automates that process. It will even handle the process of creating an encryption key in AWS KMS should you not provide one to the script.
 
-
 ## The following actions are taken in your AWS account
- - Snapshot your desired instance
- - Name the snapshot
- - Copy the snapshot to the region your RDS instance currently exists in
- - Encrypt the snapshot on copy with an encryption key
- - Rename your instance to `"RDS-OLD"`
- - Create a new encrypted instance with the original name using the encrypted snapshot
- - point any Route 53 records pointing to the old endpoint to the new
- - Remove the old RDS instance (optional)
 
-Where you once had an unencrypted RDS instance is a newly created, encrypted instance that is a replica of the original and all of the data held. 
+- Snapshot your desired instance
+- Name the snapshot
+- Copy the snapshot to the region your RDS instance currently exists in
+- Encrypt the snapshot on copy with an encryption key
+- Rename your instance to `"RDS-OLD"`
+- Create a new encrypted instance with the original name using the encrypted snapshot
+- point any Route 53 records pointing to the old endpoint to the new
+- Remove the old RDS instance (optional)
+
+Where you once had an unencrypted RDS instance is a newly created, encrypted instance that is a replica of the original and all of the data held.
 
 ## Getting Started
-A dependency of this script is having the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) configured with your credentials in `~/.aws/credentials` 
 
-```
+A dependency of this script is having the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) configured with your credentials in `~/.aws/credentials`
+
+```bash
 [default]
 aws_access_key_id = YOUR_ACCESS_KEY
 aws_secret_access_key = YOUR_SECRET_KEY
 ```
 
-Also we should configure our default region 
+Also we should configure our default region
 
-```
+```bash
 [default]
 region=us-east-1
 ```
 
 alternatively you can configure the AWS CLI by running the following
- 
-```
-$ aws configure 
+
+```bash
+aws configure
 ```
 
 ## Configuration parameters
+
 Configure these environment variables before you execute the script
+
+```bash
+export VAR=VAL
 ```
-$ export VAR=VAL
-```
 
- - RDS_INSTANCE
-   - The identifier for your RDS instance example `"test-01"`
- - KMS_KEY (Optional)
-    - If you have a KMS key you wish to use to encrypt your snapshot please reference the ID here. If not the program will create you a key called `"auto-gen-<random-string>"`
- - DELETE_OLD
-    - If set to true your original unencrypted instance will be removed. 
-
-
-
-
+- RDS_INSTANCE
+  - The identifier for your RDS instance example `"test-01"`
+- KMS_KEY (Optional)
+  - If you have a KMS key you wish to use to encrypt your snapshot please reference the ID here. If not the program will create you a key called `"auto-gen-rds-encrypt-<random-string>"`
+- DELETE_OLD
+  - If set to true your original un-encrypted instance will be removed.
--- main.py
+++ main.py
@@ -1,56 +1,118 @@
 #!/usr/bin/env python3
+
+import sys
+import random
 import os
 import boto3
 
-def produce_snapshot(instance):
+
+def check_snapshot(instance: str):
+    """ Check for the existence of a database snapshot"""
+    check_rds = rds.describe_db_snapshots(
+    DBInstanceIdentifier=f"{instance}",
+    DBSnapshotIdentifier=f"snapshot-{instance}",
+    )
+
+    return check_rds
+
+def check_kms (key: str):
+    """Check for the existence of a KMS key"""
+
+    return check_kms["KeyMetadata"]["KeyId"]
+
+def create_kms_key(key: str):
+    create_key = kms.create_key(
+        Description='Auto Created key from rds-encrypt python script',
+        KeyUsage='ENCRYPT_DECRYPT',
+        KeySpec='RSA_4096',
+        Origin='AWS_KMS',
+        BypassPolicyLockoutSafetyCheck=False,
+        Tags=[
+            {
+                'TagKey': 'CreatedBy',
+                'TagValue': 'rds-encrypt'
+            },
+        ],
+        MultiRegion=False
+    )
+
+    return create_key["KeyMetadata"]["KeyId"]
+
+def assign_kms_key_alias(key_id: str, key):
+    key_alias = kms.create_alias(
+    AliasName='string',
+    TargetKeyId='string'
+)
+
+def produce_snapshot(instance: str):
     """Create a snapshot of a desired RDS instance, name that snapshot"""
 
     # Check our snapshot does not already exist
-
+    check = check_snapshot(instance)
 
     # If the snapshot does not already exist create it
-    snapshot = rds.create_db_snapshot(
-        DBSnapshotIdentifier=f"snapshot-{instance}",
-        DBInstanceIdentifier= instance,
-    )
+    if check["DBSnapshots"] == []:
+        snapshot = rds.create_db_snapshot(
+            DBSnapshotIdentifier=f"snapshot-{instance}",
+            DBInstanceIdentifier=instance,
+        )
+    else:
+        # If the snapshot exists notify the user and they can check their AWS snapshots for this
+        # DB instance.
+        return f"Snapshot already exists at 'snapshot-{instance}'"
 
-    return snapshot
+    # Return the snapshot name from the JSON object held by the snapshot variable
+    return snapshot["DBSnapshot"]["DBSnapshotIdentifier"]
 
 
-def encrypt_snapshot(source_snapshot):
+def encrypt_snapshot(source_snapshot: str, kms_key: str):
     """Encrypt a existing RDS snapshot"""
-
+        
     encrypt = rds.copy_db_snapshot(
         SourceDBSnapshotIdentifier=source_snapshot,
-        TargetDBSnapshotIdentifier=f"encrypted-{source_snapshot}",
-        KmsKeyId="",
-        CopyTags=True,
-        OptionGroupName='string',
+        TargetDBSnapshotIdentifier=source_snapshot,
+        KmsKeyId=,
+        Tags=[
+            {
+                'Key': 'string',
+                'Value': 'string'
+            },
+        ],
         TargetCustomAvailabilityZone='string',
-        SourceRegion='string',
+        SourceRegion='string'
     )
-    
-    return encrypt
+
+    return "bob"
 
 
 if __name__ == "__main__":
-    
     # Convert the value of our OS level environment variables to python variables
-    rds_instance = os.getenv('RDS_INSTANCE')
-    user_kms_key = os.getenv('KMS_KEY')
-
-    # Check the user has configured their environment variables
-    if user_kms_key == "":
-        user_kms_key = "SCRIPT-KEY"
-    if rds_instance == "":
-        print("Please set the environment variable $RDS_INSTANCE")
-        
 
     # Initialize our boto3 endpoints
     rds = boto3.client('rds')
     kms = boto3.client('kms')
 
-    produce_snapshot(rds_instance)
+    # Get the RDS_INSTANCE environment variable from the OS if it does not exist prompt the user to set it and
+    # return exit code 1
+    rds_instance = os.getenv('RDS_INSTANCE')
+    if rds_instance == '':
+        print("Please set the 'RDS_INSTANCE' environment variable, see README.md")
+        sys.exit(1)
+
+    # Get the KMS_KEY environment variable from the OS if it is not set, configure it to our default as 
+    # specificed in README.md
+    kms_key_alias = os.getenv('KMS_KEY_ALIAS')
+    if kms_key_alias == '':
+        key_alias_suffix =  random.randint(1000, 4000)
+        kms_key_alias = f"auto-gen-rds-encrypt-{key_alias_suffix}"
+    
+    key_existence = check_kms(kms_key_alias)
+    if key_existence == '':
+        create_kms_key()
+        kms_key = assign_kms_key_alias()
 
 
 
+    snapshot_instance = produce_snapshot(rds_instance)
+
+    encrypt_instance = encrypt_snapshot(snapshot_instance, kms_key)
