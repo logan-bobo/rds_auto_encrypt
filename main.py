4aa49abe140d0f2f1181cab8ae0011796bfa2493
Logan-bobo <logan.bobofed@gmail.com>
2022-02-02 22:00:32 +0000

    functional rename and restore from encrypted snapshot

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+""" A tool used for encrypting RDS instances that were previously created in an unencrypted state"""
+import time
+import random
 import sys
 import os
 import boto3
-import time
-import random
 
 def check_kms(key: str):
     """Check for the existence of a KMS key. """
@@ -19,7 +20,7 @@ def check_database(database: str):
     """Check for the existence of the RDS instance. """
 
     check_instance = RDS.describe_db_instances(
-    DBInstanceIdentifier=database,
+        DBInstanceIdentifier=database,
     )
 
     return check_instance["DBInstances"][0]["DBInstanceIdentifier"]
@@ -28,8 +29,8 @@ def check_snapshot(instance: str):
     """ Check for the existence of a database snapshot. """
 
     check_rds = RDS.describe_db_snapshots(
-    DBInstanceIdentifier=f"{instance}",
-    DBSnapshotIdentifier=f"snapshot-{instance}",
+        DBInstanceIdentifier=f"{instance}",
+        DBSnapshotIdentifier=f"snapshot-{instance}",
     )
 
     return check_rds
@@ -80,7 +81,7 @@ def produce_snapshot(instance: str):
 
 def encrypt_snapshot(source_snapshot: str, key: str):
     """Encrypt an existing RDS snapshot. """
-        
+
     encrypt = RDS.copy_db_snapshot(
         SourceDBSnapshotIdentifier=f"{source_snapshot}",
         TargetDBSnapshotIdentifier=f"encrypted-{source_snapshot}",
@@ -89,29 +90,38 @@ def encrypt_snapshot(source_snapshot: str, key: str):
 
     print(f"Creating encrypted snapshot - encrypted-{source_snapshot}")
 
-    return encrypt
+    return encrypt["DBSnapshot"]["DBSnapshotIdentifier"]
 
 def rename_instance(instance: str):
+    """Will rename your RDS instance to have a prefix of rds-old-xxxx"""
     random_prefix_int = random.randint(1000, 9999)
     instance_new = f"rds-old-{random_prefix_int}-instance"
 
     print(f"Renaming instance - {instance_new}")
     rename = RDS.modify_db_instance(
-    DBInstanceIdentifier=instance,
-    NewDBInstanceIdentifier=instance_new,
+        DBInstanceIdentifier=instance,
+        NewDBInstanceIdentifier=instance_new,
+        ApplyImmediately=True,
     )
 
-    if rename["PendingModifiedValues"]["DBInstanceIdentifier"] != instance_new:
+    if rename["DBInstance"]["PendingModifiedValues"]["DBInstanceIdentifier"] != instance_new:
         print(f"Could not rename RDS instance {instance} to {instance_new}")
 
     update_check = None
     while update_check != instance_new:
-        check_database(instance_new)
-        print("\t - Rename not complete")
+        try:
+            update_check = check_database(instance_new)
+        except:
+            print("\t - Rename not complete")
+        time.sleep(10)
 
     print("Rename complete")
 
-    
+def restore_from_encrypted_snapshot(snapshot: str, instance_name: str):
+    RDS.restore_db_instance_from_db_snapshot(
+        DBInstanceIdentifier=instance_name,
+        DBSnapshotIdentifier=snapshot,
+    )
 
 
 if __name__ == "__main__":
@@ -120,12 +130,12 @@ if __name__ == "__main__":
     RDS = boto3.client('rds')
     KMS = boto3.client('kms')
 
-    # Get the RDS_INSTANCE environment variable from the OS if it does not exist prompt the user to 
+    # Get the RDS_INSTANCE environment variable from the OS if it does not exist prompt the user to
     # set it and return exit code 1
     RDS_INSTANCE = os.getenv('RDS_INSTANCE')
     if RDS_INSTANCE:
         rds_existence = check_database(RDS_INSTANCE)
-        if rds_existence == None:
+        if rds_existence is None:
             print("Please set the 'RDS_INSTANCE' environment variable, see README.md")
             sys.exit(1)
     else:
@@ -137,7 +147,7 @@ if __name__ == "__main__":
     kms_key = os.getenv('KMS_KEY')
     if kms_key:
         key_existence = check_kms(kms_key)
-        if key_existence == None:
+        if key_existence is None:
             print("Please set the 'KMS_KEY' environment variable, see README.md")
             sys.exit(1)
     else:
@@ -153,7 +163,6 @@ if __name__ == "__main__":
 
     # Wait for our snapshot to come available
     base_snapshot_state = check_snapshot_state(RDS_INSTANCE)
-    
     while base_snapshot_state == "creating":
         print("\t - Snapshot not ready...")
         time.sleep(20)
@@ -161,9 +170,9 @@ if __name__ == "__main__":
     print("Snapshot finished")
 
     # Encrypt our snapshot by passing in our base snapshot and encryption key
-    encrypt_snapshot(SNAPSHOT_INSTANCE, kms_key)
+    encrypted_snapshot_identifier = encrypt_snapshot(SNAPSHOT_INSTANCE, kms_key)
 
-    # Wait for our encrypted snapshot to be created. 
+    # Wait for our encrypted snapshot to be created.
     encrypted_snapshot_state = check_encrypted_snapshot_state(RDS_INSTANCE)
     while encrypted_snapshot_state == "creating":
         print("\t - Encrypted snapshot not ready...")
@@ -171,12 +180,13 @@ if __name__ == "__main__":
         encrypted_snapshot_state = check_encrypted_snapshot_state(RDS_INSTANCE)
     print("Snapshot finished")
 
-    # Renames the orignal instance RDS-OLD
-    
+    # Renames the original instance RDS-OLD
+    rename_instance(RDS_INSTANCE)
 
+    # Create a new DB instance from our snapshot
+    restore_from_encrypted_snapshot(encrypted_snapshot_identifier, RDS_INSTANCE)
 
-
-
+    # Check we now have a new encrypted instance running under the original identifier
 
 
 
