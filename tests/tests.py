9c880790b25cd48cbab6396a3e6d5f2563ce09c5
Logan Cox <mail@logan-cox.com>
2022-03-21 22:08:10 +0000

    base tests and formatting

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,40 @@
 import pytest
+from main import 
+
+class TestClass:
+
+    """main test class for rds_auto_encrypt"""
+
+    def test_check_kms():
+        
+        pass
+
+    def test_check_database():
+        pass
+
+    def test_remove_db():
+        pass
+
+    def test_check_database_encryption():
+        pass
+
+    def test_check_snapshot():
+        pass
+
+    def test_check_snapshot_state():
+        pass
+
+    def test_check_encrypted_snapshot_state():
+        pass
+
+    def test_produce_snapshot():
+        pass
+
+    def test_encrypt_snapshot():
+        pass
+
+    def test_rename_instance():
+        pass
+
+    def test_restore_from_encrypted_snapshot():
+        pass
